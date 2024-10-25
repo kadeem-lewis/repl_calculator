4a0da41fc3b17910258d5a0f2895b234f44ddc21
kadeem-lewis <email@kadeemlewis.com>
2024-10-25 17:24:31 -0400

    created base files for app and tests

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+""" Main application module. """
+class App:
+    """ Main application class. """
+    def __init__(self):
+        """ Initialize the application. """
+
+    def start(self):
+        """ Start the application. """
